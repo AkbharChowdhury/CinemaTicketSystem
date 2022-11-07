0bcde19d5db18e08ab031648050ae7ed2ad1e183
AkbharChowdhury <112480476+AkbharChowdhury@users.noreply.github.com>
2022-11-07 01:23:49 +0000

    d

--- src/App.java
+++ src/App.java
@@ -11,39 +11,28 @@ import java.io.FileNotFoundException;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class App extends JFrame implements ActionListener, KeyListener {
     static Database db;
-    DefaultListModel model;
-    JList list;
+    private final DefaultListModel model;
+    private final JList list; // the main list
 
 
+    private final JButton btnListMovies = new JButton("List Movies");
+    private final JButton btnShowTimes = new JButton("Show times");
+    private final JButton btnPurchaseTicket = new JButton("Purchase ticket");
+    private final JButton btnShowReceipt = new JButton("Show receipt");
 
+    private final JTextField txtMovieID = new JTextField(2);
+    private final JTextField txtMovieTitle = new JTextField(20);
+    private final JComboBox<String> comboBoxGenres = new JComboBox<>();
 
-    JButton btnListMovies = new JButton("List Movies");
-    JButton btnShowTimes = new JButton("Show times");
-    JButton btnPurchaseTicket = new JButton("Purchase ticket");
-    JButton btnShowReceipt = new JButton("Show receipt");
+    public App() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 
-    JTextField txtMovieID = new JTextField(2);
-    JTextField txtMovieTitle = new JTextField(20);
-    JComboBox<String> comboBoxGenres = new JComboBox<>();
-    JList<Object> movieList;
-
-
-
-
-    SpinnerModel spinnerModelChild = new SpinnerNumberModel(0, 0, 10, 1);
-
-    public App() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-//        txtTax.setEnabled(false);
-//        txtTaxable.setEnabled(false);
 
         model = new DefaultListModel();
         list = new JList(model);
-
 
         txtMovieID.addKeyListener(this);
         txtMovieTitle.addKeyListener(this);
@@ -63,9 +52,7 @@ public class App extends JFrame implements ActionListener, KeyListener {
         top.add(btnPurchaseTicket);
         top.add(btnShowReceipt);
 
-
-       populateGenreComboBox();
-
+        populateGenreComboBox();
 
         JPanel middle = new JPanel();
         middle.add(new Label("Movie Title:"));
@@ -75,7 +62,7 @@ public class App extends JFrame implements ActionListener, KeyListener {
 
         JPanel south = new JPanel();
 
-        getMovieList(false,0, "");
+        getMovieList(false, 0, "");
 
         JScrollPane movieScrollPane = new JScrollPane(list);
         south.add(movieScrollPane);
@@ -91,35 +78,7 @@ public class App extends JFrame implements ActionListener, KeyListener {
         comboBoxGenres.addActionListener(this);
 
 
-
         setVisible(true);
-    }
-
-    private void getMovieList(boolean isSearchable, int genreID, String movieTitle) {
-        for(var movie: db.showMovieList(isSearchable,genreID, movieTitle)){
-            model.addElement(MessageFormat.format("{0}, {1}, {2}",
-                    movie.getTitle(),
-                    Helper.calcDuration(movie.getDuration()),
-                    movie.getGenres()
-
-            ));
-
-//            model.addElement(movieList);
-
-
-        }
-
-
-    }
-
-    private void populateGenreComboBox() throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        // add default value
-        comboBoxGenres.addItem("Any Genre");
-        for (var genre: db.getMovieGenreList()){
-            comboBoxGenres.addItem(genre);
-        }
-
-
     }
 
     public static void main(String[] args) throws SQLException, FileNotFoundException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
@@ -128,27 +87,50 @@ public class App extends JFrame implements ActionListener, KeyListener {
 
     }
 
-    public void actionPerformed(ActionEvent e) {
-        // combo
-        if (e.getSource() == comboBoxGenres){
-            boolean isSearchable = comboBoxGenres.getSelectedIndex() !=0;
+    private static void clearList(JList list) {
+        DefaultListModel listModel = (DefaultListModel) list.getModel();
+        listModel.removeAllElements();
+    }
 
-            filterMovieList(isSearchable);
+    private void getMovieList(boolean isSearchable, int genreID, String movieTitle) {
+        for (var movie : db.showMovieList(isSearchable, genreID, movieTitle)) {
+            model.addElement(MessageFormat.format("{0}, {1}, {2}",
+                    movie.getTitle(),
+                    Helper.calcDuration(movie.getDuration()),
+                    movie.getGenres()
+            ));
+        }
+    }
 
-
+    private void populateGenreComboBox() throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        // add default value
+        comboBoxGenres.addItem("Any Genre");
+        for (var genre : db.getMovieGenreList()) {
+            comboBoxGenres.addItem(genre);
         }
 
+    }
 
-
+    public void actionPerformed(ActionEvent e) {
+        // combo
+        if (e.getSource() == comboBoxGenres) {
+            boolean isSearchable = comboBoxGenres.getSelectedIndex() != 0;
+            filterMovieList(isSearchable);
+        }
     }
 
     private void filterMovieList(boolean isSearchable) {
         clearList(list);
-        getMovieList(isSearchable, comboBoxGenres.getSelectedIndex(), txtMovieTitle.getText());
+        getMovieList(isSearchable, comboBoxGenres.getSelectedIndex(), "");
 
     }
 
+    private void filterMovieList(boolean isSearchable, String movieTitle) {
+        System.out.println("M" + movieTitle);
+        clearList(list);
+        getMovieList(isSearchable, comboBoxGenres.getSelectedIndex(), movieTitle);
 
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -157,14 +139,19 @@ public class App extends JFrame implements ActionListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getSource() == txtMovieTitle){
-            filterMovieList(true);
+
+        if (e.getSource() == txtMovieTitle) {
+            boolean isSearchable = !txtMovieTitle.getText().isEmpty();
+            if (!isSearchable) {
+                filterMovieList(false);
+            } else {
+                String title = txtMovieTitle.getText();
+                System.out.println("title is " + title);
+                filterMovieList(true, title);
+            }
+
         }
-
-
-
-
-            // listen for movie id text-field
+        // listen for movie id text-field
         if (e.getSource() == txtMovieID) {
             // link https://www.youtube.com/watch?v=cdPKsws5f-4
             char c = e.getKeyChar();
@@ -173,25 +160,15 @@ public class App extends JFrame implements ActionListener, KeyListener {
                 txtMovieID.setEditable(false);
             }
 
-            // check if number is boo
             boolean isNumber = !Character.isLetter(c);
             txtMovieID.setEditable(isNumber);
 
         }
-
-
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
-
+        
     }
-    private static void clearList(JList list){
-        DefaultListModel listModel = (DefaultListModel) list.getModel();
-        listModel.removeAllElements();
-    }
-
-
 
 }
